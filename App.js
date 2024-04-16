0fa7d53837aff47fb0bd2a44bb9cfa2078ef691d
Adalia Lee <adalialee@gmail.com>
2024-04-16 16:40:37 -0400

    more lab

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import Hello from './Hello.js';
-import Lab5 from './lab5.js';
+import Lab5 from './Laboratory5.js';
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 // import AssignmentRoutes from './Kanbas/Assignments/routes.js';
